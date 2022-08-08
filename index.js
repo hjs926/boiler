0ae03c77e04d63f7c1ad57e57b60bdf48a44f9e0
unknown <joonsun926@gmail.com>
2022-08-08 11:51:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+
+mongodb+srv://hjs926:<1234>@boiler.vqy2zh3.mongodb.net/test
+
 app.get("/", (req, res) => res.send("Hello wolrdQ!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
