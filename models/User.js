7047d08d978bea292fa14da0ff30bf385313f323
unknown <joonsun926@gmail.com>
2022-08-12 11:44:31 +0900

    update

--- models/User.js
+++ models/User.js
@@ -12,6 +12,10 @@ const userSchema = mongoose.Schema({
     trim: true,
     unique: 1,
   },
+  password: {
+    type: String,
+    minlength: 5,
+  },
   lastname: {
     type: String,
     maxlength: 50,
@@ -32,17 +36,22 @@ const userSchema = mongoose.Schema({
 userSchema.pre("save", function (next) {
   var user = this;
   //비밀번호를 암호화
-
   if (user.isModified("password")) {
     bcrypt.genSalt(saltRounds, function (err, salt) {
       if (err) return next(err);
+
       bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
         user.password = hash;
         next();
       });
     });
+  } else {
+    next();
   }
 });
+
+userSchema.methods.comparePassword;
 
 const User = mongoose.model("User", userSchema);
 
