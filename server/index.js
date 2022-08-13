096f19987fc27951e1adf458c90bf49654678910
unknown <joonsun926@gmail.com>
2022-08-13 14:59:42 +0900

    update

--- server/index.js
+++ server/index.js
@@ -26,6 +26,10 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello world"));
 
+app.get("/api/hello", (req, res) => {
+  res.send("안녕하세요");
+});
+
 app.post("/api/users/register", (req, res) => {
   //회원가입 할때 필요한 정보들을 클라이언트에서 가져오면
   //그것들을 데이터베이스에 넣어준다.
