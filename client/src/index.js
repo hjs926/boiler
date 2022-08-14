7ce359423c122b554516ac54225571ea5bf06a26
unknown <joonsun926@gmail.com>
2022-08-14 23:11:23 +0900

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,31 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import "antd/dist/antd.css";
+import { legacy_createStore as createStore } from "redux";
 import { applyMiddleware } from "redux";
 import promiseMiddleware from "redux-promise";
 import ReduxThunk from "redux-thunk";
 import Reducer from "./_reducers";
 
-const createStroeWithMiddleware = applyMiddleware(
+const createStoreWithMiddleware = applyMiddleware(
   promiseMiddleware,
   ReduxThunk
 )(createStore);
 
-ReactDOM.render(
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
+root.render(
   <Provider
-    store={createStroeWithMiddleware(
+    store={createStoreWithMiddleware(
       Reducer,
       window.__REDUX_DEVTOOLS_EXTENSION__ &&
         window.__REDUX_DEVTOOLS_EXTENSION__()
     )}
   >
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
 
 reportWebVitals();
